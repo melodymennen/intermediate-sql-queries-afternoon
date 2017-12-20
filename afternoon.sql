2cd0c9872e7df1f6b92102c33c908a0b6232d55e
Melody <Melody@Melodys-MacBook.local>
2017-12-20 15:18:18 -0700

    delete rows

--- afternoon.sql
+++ afternoon.sql
@@ -126,7 +126,11 @@ FROM Customer
 
 -- Delete rows
 
+DELETE FROM practice_delete WHERE Type = 'bronze'
 
+DELETE FROM practice_delete WHERE Type = 'silver'
+
+DELETE FROM practice_delete WHERE Value = 150
 
 -- Ecommmerce simulation 
 
